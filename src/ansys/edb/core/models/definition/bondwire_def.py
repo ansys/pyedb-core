b2dc4a6cf9011faace8b67c9b04a5ec6b50bc750
vmartidi <vasilis.martidis@ansys.com>
2022-06-06 14:45:50 +0300

    [s621567]
    bondwireDef remove setters return bool

---
+++ src/ansys/edb/core/models/definition/bondwire_def.py
@@ -0,0 +1,383 @@
+"""Bondwire Definition."""
+
+from enum import Enum
+
+import ansys.api.edb.v1.bondwire_def_pb2 as pb
+
+from ...interfaces.grpc import messages
+from ...session import (
+    get_apd_bondwire_def_stub,
+    get_bondwire_def_stub,
+    get_jedec4_bondwire_def_stub,
+    get_jedec5_bondwire_def_stub,
+)
+from ...utility.edb_errors import handle_grpc_exception
+from ..base import ObjBase
+
+
+class BondwireDefType(Enum):
+    """Enum representing bondwire types."""
+
+    APD_BONDWIRE_DEF = pb.APD_BONDWIRE_DEF
+    JEDEC4_BONDWIRE_DEF = pb.JEDEC4_BONDWIRE_DEF
+    JEDEC5_BONDWIRE_DEF = pb.JEDEC5_BONDWIRE_DEF
+
+
+class _QueryBuilder:
+    @staticmethod
+    def bondwire_def_type(type):
+        """Create a bondwire type message.
+
+        Parameters
+        ----------
+        type : BondwireDefType
+
+        Returns
+        -------
+        pb.BondwireDefTypeMessage
+        """
+        return pb.BondwireDefTypeMessage(
+            type=type,
+        )
+
+    @staticmethod
+    def bondwire_def_str_message(obj, string: str):
+        """Create a bondwire definition string message.
+
+        Parameters
+        ----------
+        obj : Union[Database, 'ApdBondwireDef', 'Jedec4BondwireDef', 'Jedec5BondwireDef']
+        string : str
+
+        Returns
+        -------
+        pb.BondwireDefStrMessage
+        """
+        return pb.BondwireDefStrMessage(object=obj.msg, name=string)
+
+
+class BondwireDef(ObjBase):
+    """Class representing a bondwire definition."""
+
+    @staticmethod
+    @handle_grpc_exception
+    def _create(msg):
+        """Create a bondwire definition.
+
+        Parameters
+        ----------
+        msg : BondwireDef
+
+        Returns
+        -------
+        BondwireDef
+        """
+        bwr = BondwireDef(msg)
+        if bwr.get_bondwire_type() == BondwireDefType.APD_BONDWIRE_DEF:
+            return ApdBondwireDef(msg)
+        elif bwr.get_bondwire_type() == BondwireDefType.JEDEC4_BONDWIRE_DEF:
+            return Jedec4BondwireDef(msg)
+        elif bwr.get_bondwire_type() == BondwireDefType.JEDEC5_BONDWIRE_DEF:
+            return Jedec5BondwireDef(msg)
+        else:
+            return None
+
+    @handle_grpc_exception
+    def delete(self):
+        """Delete a bondwire definition."""
+        get_bondwire_def_stub().Delete(self.msg)
+
+    @handle_grpc_exception
+    def get_name(self):
+        """Return the name of the bondwire definition.
+
+        Returns
+        -------
+        str
+        """
+        return get_bondwire_def_stub().GetName(self.msg)
+
+
+class ApdBondwireDef(BondwireDef):
+    """Class representing an apd bondwire definition."""
+
+    @staticmethod
+    @handle_grpc_exception
+    def create(database, name):
+        """Create a apd bondwire definition.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+
+        Returns
+        -------
+        ApdBondwireDef
+        """
+        bw_msg = get_apd_bondwire_def_stub().Create(
+            _QueryBuilder.bondwire_def_str_message(database, name)
+        )
+        return ApdBondwireDef(bw_msg)
+
+    @handle_grpc_exception
+    def load_definitions_from_file(database, name):
+        """Create an apd bondwire definition.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+        """
+        get_apd_bondwire_def_stub().LoadDefinitionsFromFile(
+            _QueryBuilder.bondwire_def_str_message(database, name)
+        )
+
+    @handle_grpc_exception
+    def find_by_name(database, name):
+        """Find apd bondwire definition by name.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+
+        Returns
+        -------
+        ApdBondwireDef
+        """
+        return ApdBondwireDef(
+            get_apd_bondwire_def_stub().FindByName(
+                _QueryBuilder.bondwire_def_str_message(database, name)
+            )
+        )
+
+    @handle_grpc_exception
+    def get_parameters(self):
+        """Get parameters of apd bondwire definition.
+
+        Returns
+        -------
+        str
+        """
+        return get_apd_bondwire_def_stub().GetParameters(self.msg)
+
+    @handle_grpc_exception
+    def set_parameters(self, name):
+        """Set parameters of apd bondwire definition.
+
+        Parameters
+        ----------
+        name: str
+        """
+        get_apd_bondwire_def_stub().SetParameters(
+            _QueryBuilder.bondwire_def_str_message(self, name)
+        )
+
+    def get_bondwire_type(self):
+        """Return apd bondwire type.
+
+        Returns
+        -------
+        BondwireDefType
+        """
+        return BondwireDefType.APD_BONDWIRE_DEF
+
+
+class _Jedec4QueryBuilder:
+    @staticmethod
+    def jedec4_bondwire_def_set_parameters_message(
+        j: "Jedec4BondwireDef", top_to_die_distance: Value
+    ):
+        return pb.Jedec4BondwireDefSetParametersMessage(
+            target=j.msg,
+            top_to_die_distance=messages.value_message(top_to_die_distance),
+        )
+
+
+class Jedec4BondwireDef(ObjBase):
+    """Class representing an jedec 4 bondwire definition."""
+
+    @staticmethod
+    @handle_grpc_exception
+    def create(database, name):
+        """Create a jedec 4 bondwire definition.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+
+        Returns
+        -------
+        Jedec4BondwireDef
+        """
+        return Jedec4BondwireDef(
+            get_jedec4_bondwire_def_stub().Create(
+                _QueryBuilder.bondwire_def_str_message(database, name)
+            )
+        )
+
+    @handle_grpc_exception
+    def find_by_name(database, name):
+        """Find jedec 4 bondwire definition by name.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+
+        Returns
+        -------
+        Jedec4BondwireDef
+        """
+        return Jedec4BondwireDef(
+            get_jedec4_bondwire_def_stub().FindByName(
+                _QueryBuilder.bondwire_def_str_message(database, name)
+            )
+        )
+
+    @handle_grpc_exception
+    def get_parameters(self):
+        """Get parameters of jedec 4 bondwire definition.
+
+        Returns
+        -------
+        Value
+        """
+        return messages.value_message_to_value(
+            get_jedec4_bondwire_def_stub().GetParameters(self.msg)
+        )
+
+    @handle_grpc_exception
+    def set_parameters(self, parameter):
+        """Set parameters of jedec 4 bondwire definition.
+
+        Parameters
+        ----------
+        parameter: Value
+        """
+        get_jedec4_bondwire_def_stub().SetParameters(
+            _Jedec4QueryBuilder.jedec4_bondwire_def_set_parameters_message(self, parameter)
+        )
+
+    @staticmethod
+    def get_bondwire_type(self):
+        """Return jedec 4 bondwire type.
+
+        Returns
+        -------
+        BondwireDefType
+        """
+        return BondwireDefType.JEDEC4_BONDWIRE_DEF
+
+
+class _Jedec5QueryBuilder:
+    @staticmethod
+    def jedec5_bondwire_def_parameters_message(
+        top_to_die_distance: Value, die_pad_angle: Value, lead_pad_angle: Value
+    ):
+        return pb.Jedec5BondwireDefParametersMessage(
+            top_to_die_distance=messages.value_message(top_to_die_distance),
+            die_pad_angle=messages.value_message(die_pad_angle),
+            lead_pad_angle=messages.value_message(lead_pad_angle),
+        )
+
+    @staticmethod
+    def jedec5_bondwire_def_set_parameters_message(
+        j: "Jedec5BondwireDef",
+        top_to_die_distance: Value,
+        die_pad_angle: Value,
+        lead_pad_angle: Value,
+    ):
+        return pb.Jedec5BondwireDefSetParametersMessage(
+            target=j.msg,
+            parameters=_Jedec5QueryBuilder.jedec5_bondwire_def_parameters_message(
+                top_to_die_distance, die_pad_angle, lead_pad_angle
+            ),
+        )
+
+
+class Jedec5BondwireDef(BondwireDef):
+    """Class representing an jedec 4 bondwire definition."""
+
+    @staticmethod
+    @handle_grpc_exception
+    def create(database, name):
+        """Create a jedec 5 bondwire definition.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+
+        Returns
+        -------
+        Jedec5BondwireDef
+        """
+        return Jedec5BondwireDef(
+            get_jedec5_bondwire_def_stub().Create(
+                _QueryBuilder.bondwire_def_str_message(database, name)
+            )
+        )
+
+    @handle_grpc_exception
+    def find_by_name(database, name):
+        """Find jedec 5 bondwire definition by name.
+
+        Parameters
+        ----------
+        database: Database,
+        name: str
+
+        Returns
+        -------
+        Jedec5BondwireDef
+        """
+        return Jedec5BondwireDef(
+            get_jedec5_bondwire_def_stub().FindByName(
+                _QueryBuilder.bondwire_def_str_message(database, name)
+            )
+        )
+
+    @handle_grpc_exception
+    def get_parameters(self):
+        """Get parameters of jedec 5 bondwire definition.
+
+        Returns
+        -------
+        Tuple[Value, Value, Value]
+        """
+        get_parameters_msg = get_jedec5_bondwire_def_stub().GetParameters(self.msg)
+        return (
+            messages.value_message_to_value(get_parameters_msg.top_to_die_distance),
+            messages.value_message_to_value(get_parameters_msg.die_pad_angle),
+            messages.value_message_to_value(get_parameters_msg.lead_pad_angle),
+        )
+
+    @handle_grpc_exception
+    def set_parameters(self, ttd, dpa, lpa):
+        """Set parameters of jedec 5 bondwire definition.
+
+        Parameters
+        ----------
+        ttd: Value
+            Value for top to die distance parameter
+        dpa: Value
+            Value for die pad angle parameter
+        lpa: Value
+            Value for lead pad angle parameter
+        """
+        get_jedec5_bondwire_def_stub().SetParameters(
+            _Jedec5QueryBuilder.jedec5_bondwire_def_set_parameters_message(self, ttd, dpa, lpa)
+        )
+
+    @staticmethod
+    def get_bondwire_type(self):
+        """Return jedec 5 bondwire type.
+
+        Returns
+        -------
+        BondwireDefType
+        """
+        return BondwireDefType.JEDEC5_BONDWIRE_DEF

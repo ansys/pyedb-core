728bf06e48da12e0c7e1c8f8148f8adea8da17ea
Karan Bhagat <karan.bhagat@ansys.com>
2022-08-11 18:25:55 -0400

    T688009: Consolidating pip install + various fixes (Windows, env. var detection and setting)

--- test_pyedb.sh
+++ test_pyedb.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+current_script_dir=$(dirname $0)
+pushd ${current_script_dir}
+current_script_dir=${PWD}
+
+isWindows=true
+isLinux=false
 # Set up the python virtual environment
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    python -m venv .venv && source .venv/bin/activate
+if [[ "$(uname -s)" = "Linux" ]]; then
+  isWindows=false
+  isLinux=true
+  python3 -m venv .venv && source .venv/bin/activate
 else
-    python -m venv .venv
-    .venv/Scripts/activate.bat
+  python -m venv .venv && source .venv/Scripts/activate
 fi
 
 # Set up the .env.test file so we know where to find the install
-echo "RPC_SERVER_ROOT="$RPC_SERVER_ROOT > .env.test
+env_test=${current_script_dir}/.env.test
+if [ ! -f "${env_test}" ]; then
+  echo "Creating ${env_test} file"
+  cp .env.test.example "${env_test}" -rp
+  if [[ -z "${RPC_SERVER_ROOT}" ]]; then
+    echo "***[ERROR]: Environment variable \$RPC_SERVER_ROOT is not set. Please add this to ${env_test} ***";
+    exit 1
+  fi
+  if ${isWindows}; then
+    RPC_SERVER_ROOT="${RPC_SERVER_ROOT//\\//}/"
+  fi
+  sed -i "s|RPC_SERVER_ROOT|RPC_SERVER_ROOT=${RPC_SERVER_ROOT}|g" "${env_test}"
+fi
 
 # Run tox
-python -m pip install --upgrade pip flit tox tox-gh-actions
-tox -e test -- --junitxml=junit/test-results.xml
+python -m pip install --upgrade -r requirements/requirements_tox.txt
+python -m pip install --upgrade tox-gh-actions
+python -m tox -e test
+
+popd

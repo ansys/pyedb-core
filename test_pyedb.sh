c15e9c5f0a0717cd25656af2aef40f140ac5783e
drewm102 <103221973+drewm102@users.noreply.github.com>
2022-08-16 13:08:58 -0600

    T688009: Added basic script for running tox tests (#73)
    * T688009: Added basic script for running tox tests
    
    * T688009: Consolidating pip install + various fixes (Windows, env. var detection and setting)
    
    * Removing un-required package for local testing
    
    Co-authored-by: Karan Bhagat <karan.bhagat@ansys.com>

---
+++ test_pyedb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+current_script_dir=$(dirname $0)
+pushd ${current_script_dir}
+current_script_dir=${PWD}
+
+isWindows=true
+isLinux=false
+# Set up the python virtual environment
+if [[ "$(uname -s)" = "Linux" ]]; then
+  isWindows=false
+  isLinux=true
+  python3 -m venv .venv && source .venv/bin/activate
+else
+  python -m venv .venv && source .venv/Scripts/activate
+fi
+
+# Set up the .env.test file so we know where to find the install
+env_test=${current_script_dir}/.env.test
+if [ ! -f "${env_test}" ]; then
+  echo "Creating ${env_test} file"
+  cp .env.test.example "${env_test}" -rp
+  if [[ -z "${RPC_SERVER_ROOT}" ]]; then
+    echo "***[ERROR]: Environment variable \$RPC_SERVER_ROOT is not set. Please add this to ${env_test} ***";
+    exit 1
+  fi
+  if ${isWindows}; then
+    RPC_SERVER_ROOT="${RPC_SERVER_ROOT//\\//}/"
+  fi
+  sed -i "s|RPC_SERVER_ROOT|RPC_SERVER_ROOT=${RPC_SERVER_ROOT}|g" "${env_test}"
+fi
+
+# Run tox
+python -m pip install --upgrade -r requirements/requirements_tox.txt
+python -m tox -e test
+
+popd

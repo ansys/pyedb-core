9e45d15d287ead837f9d02625dcb3f8ef07ae70a
Hiroki Takezawa <hiro727@users.noreply.github.com>
2022-08-30 16:09:45 -0700

    T696694: Add net api

--- src/ansys/edb/core/layout_obj.py
+++ src/ansys/edb/core/layout_obj.py
@@ -6,6 +6,7 @@ from ansys.edb.core import ObjBase
 import ansys.edb.core.messages as messages
 from ansys.edb.edb_defs import LayoutObjType
 from ansys.edb.layout import layout
+from ansys.edb.session import LayoutObjServiceStub, StubAccessor, StubType
 
 
 class _QueryBuilder:
@@ -38,6 +39,7 @@ class _QueryBuilder:
 class LayoutObj(ObjBase):
     """Layout Object class."""
 
+    __stub: LayoutObjServiceStub = StubAccessor(StubType.layout_obj)
     layout_obj_type = LayoutObjType.INVALID_LAYOUT_OBJ
 
     @property
@@ -46,9 +48,9 @@ class LayoutObj(ObjBase):
 
         Returns
         -------
-            LayoutObjType enum of the Layout Object.
+            :class:`LayoutObjType <ansys.edb.edb_defs.LayoutObjType>`
         """
-        return self.layout_type
+        return self.layout_obj_type
 
     @property
     def layout(self):
@@ -56,44 +58,46 @@ class LayoutObj(ObjBase):
 
         Returns
         -------
-            Layout of the Layout Object.
+            :class:`Layout <ansys.edb.layout.Layout>`
         """
         return layout.Layout(
-            self.__stub.GetLayout(_QueryBuilder.layout_obj_target_msg(self, self.layout_type))
+            self.__stub.GetLayout(_QueryBuilder.layout_obj_target_msg(self, self.layout_obj_type))
         )
 
     def delete(self):
         """Delete the layout object."""
-        self.__stub.Delete(_QueryBuilder.layout_obj_target_msg(self, self.layout_type))
+        self.__stub.Delete(_QueryBuilder.layout_obj_target_msg(self, self.layout_obj_type))
 
-    def get_product_property(self, prod_id, attr_it):
+    def get_product_property(self, prod_id, attr_id):
         """Get the product property of the layout object with the given product and attribute ids.
 
         Parameters
         ----------
-        prod_id : ProductIdType
-        attr_it : int
+        prod_id : :class:`ProductIdType <ansys.edb.database.ProductIdType>`
+        attr_id : int
 
         Returns
         -------
         str
         """
         return self.__stub.GetProductProperty(
-            _QueryBuilder.get_product_property_type_msg(self, prod_id, attr_it, self.layout_type)
+            _QueryBuilder.get_product_property_type_msg(
+                self, prod_id, attr_id, self.layout_obj_type
+            )
         ).value
 
-    def set_product_property(self, prod_id, att_id, prop_value):
+    def set_product_property(self, prod_id, attr_id, prop_value):
         """Set the product property of the layout object associated with the given product and attribute ids.
 
         Parameters
         ----------
-        prod_id : ProductIdType
-        attr_it : int
+        prod_id : :class:`ProductIdType <ansys.edb.database.ProductIdType>`
+        attr_id : int
         prop_value : str
         """
         self.__stub.SetProductProperty(
             _QueryBuilder.set_product_property_type_msg(
-                self, prod_id, att_id, prop_value, self.layout_type
+                self, prod_id, attr_id, prop_value, self.layout_obj_type
             )
         )
 
@@ -102,13 +106,13 @@ class LayoutObj(ObjBase):
 
         Parameters
         ----------
-        prod_id : ProductIdType
+        prod_id : :class:`ProductIdType <ansys.edb.database.ProductIdType>`
 
         Returns
         -------
         list[int]
         """
         attr_ids = self.__stub.GetProductPropertyIds(
-            _QueryBuilder.get_product_property_ids_type_msg(self, prod_id, self.layout_type)
+            _QueryBuilder.get_product_property_ids_type_msg(self, prod_id, self.layout_obj_type)
         ).ids
         return [attr_id for attr_id in attr_ids]

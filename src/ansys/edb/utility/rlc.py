c0c2ebafbbb53afce519d418a5070981c41d51ae
vmartidi <vasilis.martidis@ansys.com>
2022-09-02 17:25:22 +0300

    i93: Rlc properties setters #93

--- src/ansys/edb/utility/rlc.py
+++ src/ansys/edb/utility/rlc.py
@@ -50,6 +50,17 @@ class Rlc(ObjBase):
         """
         return self._r if self._msg is None else Value(self._msg.r)
 
+    @r.setter
+    def r(self, resistance):
+        """Set resistance value.
+
+        Parameters
+        ----------
+        resistance
+            Resistance to be set.
+        """
+        self._r = resistance
+
     @property
     def l(self):
         """Return induction value.
@@ -60,6 +71,17 @@ class Rlc(ObjBase):
             Impedance value in Henries.
         """
         return self._l if self._msg is None else Value(self._msg.l)
+
+    @l.setter
+    def l(self, induction):
+        """Set induction value.
+
+        Parameters
+        ----------
+        induction
+            Induction to be set.
+        """
+        self._l = induction
 
     @property
     def c(self):
@@ -72,6 +94,17 @@ class Rlc(ObjBase):
         """
         return self._c if self._msg is None else Value(self._msg.c)
 
+    @c.setter
+    def c(self, capacitance):
+        """Set capacitance value.
+
+        Parameters
+        ----------
+        capacitance
+            Capacitance to be set.
+        """
+        self._c = capacitance
+
     @property
     def r_enabled(self):
         """Return whether resistance is enabled.
@@ -81,6 +114,17 @@ class Rlc(ObjBase):
         bool
         """
         return self._r_enabled if self._msg is None else self._msg.r_enabled.value
+
+    @r_enabled.setter
+    def r_enabled(self, resistance_enabled):
+        """Set whether resistance is_enabled value.
+
+        Parameters
+        ----------
+        resistance_enabled : bool
+            Enabled/Disable resistance.
+        """
+        self._r_enabled = resistance_enabled
 
     @property
     def l_enabled(self):
@@ -92,6 +136,17 @@ class Rlc(ObjBase):
         """
         return self._l_enabled if self._msg is None else self._msg.l_enabled.value
 
+    @l_enabled.setter
+    def l_enabled(self, induction_enabled):
+        """Set whether induction is_enabled value.
+
+        Parameters
+        ----------
+        induction_enabled : bool
+            Enabled/Disable induction.
+        """
+        self._l_enabled = induction_enabled
+
     @property
     def c_enabled(self):
         """Return whether capacitance is enabled.
@@ -102,6 +157,17 @@ class Rlc(ObjBase):
         """
         return self._c_enabled if self._msg is None else self._msg.c_enabled.value
 
+    @c_enabled.setter
+    def c_enabled(self, capacitance_enabled):
+        """Set whether capacitance is_enabled value.
+
+        Parameters
+        ----------
+        capacitance_enabled : bool
+            Enabled/Disable capacitance.
+        """
+        self._c_enabled = capacitance_enabled
+
     @property
     def is_parallel(self):
         """Return if parallel.
@@ -111,3 +177,14 @@ class Rlc(ObjBase):
         bool
         """
         return self._is_parallel if self._msg is None else self._msg.is_parallel.value
+
+    @is_parallel.setter
+    def is_parallel(self, is_parallel):
+        """Set parallel.
+
+        Parameters
+        ----------
+        is_parallel : bool
+            Define is_parallel.
+        """
+        self._is_parallel = is_parallel

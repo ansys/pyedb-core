f66a0703baefc5b50bafd16d31553017cf8375eb
vmartidis <103631676+vmartidis@users.noreply.github.com>
2022-09-09 18:49:07 +0300

    D108: PadstackDefData Get/Set hole parameters minor addition to the documentation #108 (#113)
    * i108: Get/Set hole parameters minor addition to the documentation #108
    
    * i108: Get/Set hole parameters minor addition to the documentation #108

--- src/ansys/edb/definition/padstack_def_data.py
+++ src/ansys/edb/definition/padstack_def_data.py
@@ -331,7 +331,7 @@ class PadstackDefData(ObjBase):
 
         Parameters
         ----------
-        layer : Union[str, int]
+        layer : Union[str, int, None]
         pad_type : PadstackDefData.PadType
 
         Returns
@@ -370,7 +370,7 @@ class PadstackDefData(ObjBase):
 
         Parameters
         ----------
-        layer : Union[str, int]
+        layer : Union[str, int, None]
             Layer name.
         pad_type : PadstackDefData.PadType
             Pad type.

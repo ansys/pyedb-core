0731bc659519f685c688ba8b365fe5ff71ce0a7f
dmiller <drew.miller@ansys.com>
2022-09-28 09:39:42 -0600

    T694957: is_null is a property now

--- tests/e2e/unit_tests/test_cell.py
+++ tests/e2e/unit_tests/test_cell.py
@@ -1,10 +1,10 @@
 def test_get_database(circuit_cell, new_database):
     cell_db = circuit_cell.database
-    assert not cell_db.is_null()
+    assert not cell_db.is_null
     assert cell_db.id == new_database.id
 
 
 def test_get_layout(circuit_cell):
     cell_lyt = circuit_cell.layout
-    assert not cell_lyt.is_null()
+    assert not cell_lyt.is_null
     assert circuit_cell.id == cell_lyt.cell.id

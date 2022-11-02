325bfac8a4ba97fbef69c5dac19d28d815a88076
vmartidi <vasilis.martidis@ansys.com>
2022-11-02 11:36:10 +0200

    S641097: add enum to init.py

--- src/ansys/edb/definition/__init__.py
+++ src/ansys/edb/definition/__init__.py
@@ -18,7 +18,10 @@ from ansys.edb.definition.component_property import ComponentProperty
 from ansys.edb.definition.dataset_def import DatasetDef
 from ansys.edb.definition.debye_model import DebyeModel
 from ansys.edb.definition.die_property import DieOrientation, DieProperty, DieType
-from ansys.edb.definition.dielectric_material_model import DielectricMaterialModel
+from ansys.edb.definition.dielectric_material_model import (
+    DielectricMaterialModel,
+    DielectricMaterialModelType,
+)
 from ansys.edb.definition.djordjecvic_sarkar_model import DjordjecvicSarkarModel
 from ansys.edb.definition.ic_component_property import ICComponentProperty
 from ansys.edb.definition.material_def import MaterialDef, MaterialProperty, ThermalModifier

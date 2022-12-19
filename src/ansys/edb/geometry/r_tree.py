4e0b9ca55a3a1831adc0ff8bdd896f8fb62d1d22
vmartidi <vasilis.martidis@ansys.com>
2022-12-19 12:59:55 +0200

    RTree: pr update

--- src/ansys/edb/geometry/r_tree.py
+++ src/ansys/edb/geometry/r_tree.py
@@ -27,15 +27,6 @@ class _QueryBuilder:
         )
 
     @staticmethod
-    def r_tree_poly_message(rtree, polygon, prop_id):
-        """Create an RTreePolyMessage."""
-        return pb.RTreePolyMessage(
-            target=messages.edb_obj_message(rtree),
-            polygon=messages.edb_obj_message(polygon),
-            id=prop_id,
-        )
-
-    @staticmethod
     def r_tree_geometry_request_message(rtree, polygon, prop_id, increment_visit):
         """Create an RTreeGeometryRequestMessage."""
         return pb.RTreeGeometryRequestMessage(
@@ -61,7 +52,7 @@ class RTree(ObjBase):
 
             Parameters
             ----------
-            polygon: :class:`PolygonData <ansys.edb.primitive.Polygon>`
+            polygon: :class:`PolygonData <ansys.edb.geometry.PolygonData>`
                 The polygon representation for the object in the spatial index.
             obj: ObjBase
                 The object to be stored in the index.
@@ -71,10 +62,20 @@ class RTree(ObjBase):
             self.obj = obj
 
     __stub: r_tree_pb2_grpc.RTreeServiceStub = StubAccessor(StubType.r_tree)
-    __rtree_obj_dict = None
-    __unique_id = None
 
-    def create(self, tolerance=1e-9):
+    def __init__(self, msg):
+        """Init method for RTree.
+
+        Parameters
+        ----------
+        msg
+        """
+        super().__init__(msg)
+        self.__rtree_obj_dict = {}
+        self.__unique_id = None
+
+    @classmethod
+    def create(cls, tolerance=1e-9):
         """Create an RTree.
 
         Parameters
@@ -87,14 +88,18 @@ class RTree(ObjBase):
         RTree
             The new RTree created.
         """
-        self.rtree_obj_dict = {}
-        self.unique_id = 1
-        return RTree(self.__stub.Create(messages.double_message(tolerance)))
+        rtree_created = RTree(cls.__stub.Create(messages.double_message(tolerance)))
+        return rtree_created
 
-    def increase_unique_id(self):
-        """Increase RTree unique id counter."""
-        self.unique_id += 1
-        return self.unique_id
+    def __handle_rtree_obj(self, rtree_obj):
+        if rtree_obj.__unique_id is None:
+            for i in self.__rtree_obj_dict.keys():
+                if self.__rtree_obj_dict[i].polygon == rtree_obj.polygon:
+                    rtree_obj.__unique_id = self.__rtree_obj_dict[i].__unique_id
+                    return True
+            return False
+        else:
+            return True
 
     @property
     @parser.to_box
@@ -110,12 +115,13 @@ class RTree(ObjBase):
         rtree_obj: RTreeObj
             An R-tree data object, with index.
         """
-        self.increase_unique_id()
-        rtree_obj.__unique_id = self.unique_id
-        self.rtree_obj_dict[rtree_obj.__unique_id] = rtree_obj
+        unique_id = 1 if self.__unique_id is None else self.__unique_id + 1
         self.__stub.InsertIntObject(
-            _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
+            _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, unique_id)
         )
+        self.__unique_id = unique_id
+        rtree_obj.__unique_id = int(unique_id)
+        self.__rtree_obj_dict[unique_id] = rtree_obj
 
     def delete(self, rtree_obj):
         """Delete RTreeObj from the RTree object.
@@ -125,10 +131,11 @@ class RTree(ObjBase):
         rtree_obj: RTreeObj
             An R-tree data object, with index.
         """
-        self.__stub.DeleteIntObject(
-            _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
-        )
-        del self.rtree_obj_dict[rtree_obj.__unique_id]
+        if self.__handle_rtree_obj(rtree_obj):
+            self.__stub.DeleteIntObject(
+                _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
+            )
+            del self.__rtree_obj_dict[rtree_obj.__unique_id]
 
     def empty(self):
         """Check if the RTree is contains no geometry.
@@ -138,18 +145,19 @@ class RTree(ObjBase):
         bool
         """
         msg_empty = self.__stub.Empty(self.msg).value
-        return (len(self.rtree_obj_dict) == 0) == msg_empty
+        return msg_empty
 
     def search(self, box, bb_search):
         """Search all objects intersecting the given box.
 
         Parameters
         ----------
-        box: box
+        box: Tuple[:class:`PointData <geometry.PointData>, :class:`PointData <geometry.PointData>]
             The testing region, described as a (lower-left, upper-right) box.
         bb_search: bool
-            If true, an RTreeObj intersects when the bounding-box of it's PolygonData intersects the testing \
-        object. If false, an explicit intersection is required for a hit.
+            If true, an RTreeObj intersects when the bounding-box of it's \
+            :class:`PolygonData <ansys.edb.geometry.PolygonData>` intersects the testing  object. If false, an explicit\
+             intersection is required for a hit.
 
         Returns
         -------
@@ -157,7 +165,7 @@ class RTree(ObjBase):
             A list of intersecting RTreeObj.
         """
         msg = self.__stub.Search(_QueryBuilder.r_tree_search_message(self, box, bb_search))
-        return [self.rtree_obj_dict[int(to_id)] for to_id in msg.props]
+        return [self.__rtree_obj_dict[int(to_id)] for to_id in msg.props]
 
     def nearest_neighbor(self, rtree_obj):
         """Find the nearest-neighbor of the given RTree object (polygon, id pair).
@@ -174,10 +182,11 @@ class RTree(ObjBase):
         tuple[geometry.PointData, geometry.PointData]: A line-segment spanning the closest points between obj and \
         nearest.
         """
-        msg = self.__stub.NearestNeighbor(
-            _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
-        )
-        return self.rtree_obj_dict[msg.id], parser.to_box(msg.coordinates)
+        if self.__handle_rtree_obj(rtree_obj):
+            msg = self.__stub.NearestNeighbor(
+                _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
+            )
+            return self.__rtree_obj_dict[int(msg.id)], parser.to_box(msg.coordinates)
 
     def touching_geometry(self, rtree_obj, increment_visit):
         """Find all geometry touching the provided RTree object (polygon, id pair).  Note that the  provided RTree \
@@ -195,12 +204,13 @@ class RTree(ObjBase):
         :obj:`list` of RTreeObj
             All touching RTree objects.
         """
-        msg = self.__stub.TouchingGeometry(
-            _QueryBuilder.r_tree_geometry_request_message(
-                self, rtree_obj.polygon, rtree_obj.__unique_id, increment_visit
+        if self.__handle_rtree_obj(rtree_obj):
+            msg = self.__stub.TouchingGeometry(
+                _QueryBuilder.r_tree_geometry_request_message(
+                    self, rtree_obj.polygon, rtree_obj.__unique_id, increment_visit
+                )
             )
-        )
-        return [self.rtree_obj_dict[int(to_id)] for to_id in msg.props]
+            return [self.__rtree_obj_dict[int(to_id)] for to_id in msg.props]
 
     def connected_geometry(self, rtree_obj, increment_visit):
         """Find connected geometry.  Note that, if connections exists, the provided RTree object \
@@ -217,12 +227,13 @@ class RTree(ObjBase):
         :obj:`list` of RTreeObj
             The connected geometry list.
         """
-        msg = self.__stub.ConnectedGeometry(
-            _QueryBuilder.r_tree_geometry_request_message(
-                self, rtree_obj.polygon, rtree_obj.__unique_id, increment_visit
+        if self.__handle_rtree_obj(rtree_obj):
+            msg = self.__stub.ConnectedGeometry(
+                _QueryBuilder.r_tree_geometry_request_message(
+                    self, rtree_obj.polygon, rtree_obj.__unique_id, increment_visit
+                )
             )
-        )
-        return [self.rtree_obj_dict[int(to_id)] for to_id in msg.props]
+            return [self.__rtree_obj_dict[int(to_id)] for to_id in msg.props]
 
     @property
     def connected_geometry_sets(self):
@@ -234,7 +245,7 @@ class RTree(ObjBase):
         for set_size in range(0, len(msg.sizes) - 1):
             rtree_obj_set = []
             for j in range(set_start, set_size):
-                rtree_obj_set.append(self.rtree_obj_dict[msg.id[j]])
+                rtree_obj_set.append(self.__rtree_obj_dict[msg.id[j]])
             set_start += set_size
             rtree_obj_sets.append(rtree_obj_set)
         return rtree_obj_sets
@@ -255,9 +266,10 @@ class RTree(ObjBase):
         -------
         bool
         """
-        return self.__stub.IsVisited(
-            _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
-        ).value
+        if self.__handle_rtree_obj(rtree_obj):
+            return self.__stub.IsVisited(
+                _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
+            ).value
 
     def visit(self, rtree_obj):
         """Increment a given RTree object (polygon, id pair) count.
@@ -267,9 +279,10 @@ class RTree(ObjBase):
         rtree_obj: RTreeObj
             An R-tree data object, with index.
         """
-        self.__stub.Visit(
-            _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
-        )
+        if self.__handle_rtree_obj(rtree_obj):
+            self.__stub.Visit(
+                _QueryBuilder.r_tree_obj_message(self, rtree_obj.polygon, rtree_obj.__unique_id)
+            )
 
     @property
     def get_visit(self):

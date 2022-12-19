94ca9396f12929b91d8f455af20c7a68f0c24097
vmartidi <vasilis.martidis@ansys.com>
2022-12-19 16:29:53 +0200

    s641114: update

--- src/ansys/edb/utility/material_property_thermal_modifier_params.py
+++ src/ansys/edb/utility/material_property_thermal_modifier_params.py
@@ -18,7 +18,7 @@ class BasicQuadraticParams:
 
     def __init__(
         self,
-        temp_ref_val=Value("22cel"),
+        temp_ref_val="22cel",
         c1_val=Value(0),
         c2_val=Value(0),
     ):
@@ -33,7 +33,7 @@ class BasicQuadraticParams:
         c2_val : str, int, float, complex, Value
             The C2 value in the quadratic model.
         """
-        self.temp_ref_val = temp_ref_val
+        self.temp_ref_val = Value(temp_ref_val)
         self.c1_val = c1_val
         self.c2_val = c2_val
 
@@ -58,8 +58,8 @@ class AdvancedQuadraticParams:
 
     def __init__(
         self,
-        temp_lower_limit_val=Value("-273.15cel"),
-        temp_upper_limit_val=Value("1000cel"),
+        temp_lower_limit_val="-273.15cel",
+        temp_upper_limit_val="1000cel",
         auto_calc_constant_thermal_modifier_vals=True,
         lower_constant_thermal_modifier_val=1,
         upper_constant_thermal_modifier_val=1,
@@ -80,8 +80,8 @@ class AdvancedQuadraticParams:
         upper_constant_thermal_modifier_val : str, int, float, complex, Value
             The constant thermal modifier value for temperatures greater than upper_constant_thermal_modifier_val.
         """
-        self.temp_lower_limit_val = temp_lower_limit_val
-        self.temp_upper_limit_val = temp_upper_limit_val
+        self.temp_lower_limit_val = Value(temp_lower_limit_val)
+        self.temp_upper_limit_val = Value(temp_upper_limit_val)
         self.auto_calc_constant_thermal_modifier_vals = auto_calc_constant_thermal_modifier_vals
         self.lower_constant_thermal_modifier_val = lower_constant_thermal_modifier_val
         self.upper_constant_thermal_modifier_val = upper_constant_thermal_modifier_val

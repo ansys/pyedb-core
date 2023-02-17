cf0b51a779b8f27c2fb5980c48e9fc7d67204410
vmartidi <vasilis.martidis@ansys.com>
2023-02-17 13:37:42 +0200

    S64112: Transform3D Service update doc

--- src/ansys/edb/utility/transform3d.py
+++ src/ansys/edb/utility/transform3d.py
@@ -46,7 +46,7 @@ class Transform3D(ObjBase):
 
     @classmethod
     def create_identity(cls):
-        """Create a Transform3D.
+        """Create an identity transformation 3d matrix.
 
         Returns
         -------
@@ -65,59 +65,82 @@ class Transform3D(ObjBase):
         return Transform3D(cls.__stub.CreateCopy(messages.edb_obj_message(transform3d)))
 
     @classmethod
-    def create_offset(cls, point3d):
-        """Create a Transform3D with offset.
+    def create_offset(cls, matrix):
+        """Create from general matrix data.
 
         Parameters
         ----------
-        point3d : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
-
+        matrix : :obj:`list` of :obj:`list` of :obj:`floats`
+            The 4x4 array to copy from.
         Returns
         -------
         Transform3D
         """
-        return Transform3D(cls.__stub.CreateOffset(messages.cpos_3d_message(point3d)))
+        trans = Transform3D(cls.__stub.CreateIdentity(empty_pb2.Empty()))
+        trans.matrix = matrix
+        return trans
 
     @classmethod
-    def create_center_scale(cls, point3d, scale):
-        """Create a Transform3D with center and scale.
+    def create_offset(cls, offset):
+        """Create a Transform3D with offset.
 
         Parameters
         ----------
-        point3d : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+        offset : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            The vector offset.
+        Returns
+        -------
+        Transform3D
+        """
+        return Transform3D(cls.__stub.CreateOffset(messages.cpos_3d_message(offset)))
+
+    @classmethod
+    def create_center_scale(cls, center, scale):
+        """Create a Transform3D for scaling about a point.
+
+        Parameters
+        ----------
+        center : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            The center of the transformation.
         scale : :obj:`float`
+            The scale factor of the transformation.
 
         Returns
         -------
         Transform3D
         """
         return Transform3D(
-            cls.__stub.CreateCenterScale(messages.cpos_3d_double_message(point3d, scale))
+            cls.__stub.CreateCenterScale(messages.cpos_3d_double_message(center, scale))
         )
 
     @classmethod
-    def create_rotation_from_angle(cls, point3d):
-        """Create a Transform3D with rotation from angle.
+    def create_rotation_from_angle(cls, zyx_decomposition):
+        """Create a Transform3D from zyx decomposition.
 
         Parameters
         ----------
-        point3d : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
-
+        zyx_decomposition : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+             ZYX decomposition.
         Returns
         -------
         Transform3D
         """
-        return Transform3D(cls.__stub.CreateRotationFromAngle(messages.cpos_3d_message(point3d)))
+        return Transform3D(
+            cls.__stub.CreateRotationFromAngle(messages.cpos_3d_message(zyx_decomposition))
+        )
 
     @classmethod
     def create_rotation_from_axis(cls, x, y, z):
-        """Create a Transform3D with rotation from axis.
+        """Create a Transform3D with rotation matrix from 3 axis.
 
         Parameters
         ----------
         x : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            X axis.
         y : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            Y axis.
         z : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            Z axis.
 
         Returns
         -------
@@ -128,58 +151,60 @@ class Transform3D(ObjBase):
         )
 
     @classmethod
-    def create_rotationfrom_axis_and_angle(cls, point3d, angle):
+    def create_rotationfrom_axis_and_angle(cls, axis, angle):
         """Create a Transform3D with axis and angle.
 
         Parameters
         ----------
-        point3d : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+        axis : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            Axis.
         angle : :obj:`float`
+            Angle.
 
         Returns
         -------
         Transform3D
         """
         return Transform3D(
-            cls.__stub.CreateRotationFromAxisAndAngle(
-                messages.cpos_3d_double_message(point3d, angle)
-            )
+            cls.__stub.CreateRotationFromAxisAndAngle(messages.cpos_3d_double_message(axis, angle))
         )
 
     @classmethod
-    def create_rotation_from_to_axis(cls, from_point3d, to_point3d):
+    def create_rotation_from_to_axis(cls, from_axis, to_axis):
         """Create a Transform3D with rotation from to axis.
 
         Parameters
         ----------
-        from_point3d : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
-        to_point3d : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+        from_axis : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            From axis.
+        to_axis : :class:`Point3DData <ansys.edb.geometry.Point3DData>`
+            To axis.
 
         Returns
         -------
         Transform3D
         """
         return Transform3D(
-            cls.__stub.CreateRotationFromToAxis(
-                messages.cpos_3d_pair_message(from_point3d, to_point3d)
-            )
+            cls.__stub.CreateRotationFromToAxis(messages.cpos_3d_pair_message(from_axis, to_axis))
         )
 
     @classmethod
-    def create_transform_2d(cls, transform_2d, z_off):
-        """Create a Transform3D with Transform2D data.
+    def create_transform_2d(cls, transform, z_off):
+        """Create a Transform3D with Transform data.
 
         Parameters
         ----------
-        transform_2d : :class:`Point3DData <ansys.edb.utility.Transform>`
+        transform : :class:`Point3DData <ansys.edb.utility.Transform>`
+            Transform data.
         z_off : :obj:`float`
+            Z offset.
 
         Returns
         -------
         Transform3D
         """
         return Transform3D(
-            cls.__stub.CreateTransform2D(messages.double_property_message(transform_2d, z_off))
+            cls.__stub.CreateTransform2D(messages.double_property_message(transform, z_off))
         )
 
     def transpose(self):
@@ -283,7 +308,7 @@ class Transform3D(ObjBase):
 
     @property
     def matrix(self):
-        """:obj:`list` of :obj:`list` of :obj:`floats`: 2 dimensional 4x4 array."""
+        """:obj:`list` of :obj:`list` of :obj:`floats` : Transformation matrix as a 2D 4x4 Array."""
         msg = self.__stub.GetMatrix(messages.edb_obj_message(self))
         matrix = [[float(_) for _ in msg.doubles[(i - 1) * 4 : i * 4]] for i in range(1, 5)]
         return matrix

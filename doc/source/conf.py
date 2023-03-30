b625457520e47232893df4936f035364b5a6b782
bwnedrud <92535822+bwnedrud@users.noreply.github.com>
2023-03-30 16:40:48 -0400

    T227: Convert documentation to versioned deployment (#243)
    * T227: Convert documentation to versioned deployment
    
    * T227: Convert style and docs check to pyansys actions
    
    * T227: Add vale config
    
    * T227: Fix spells
    
    * T227: Add vale spell check rules
    
    * T227: Build doc
    
    * T227: Build with tox for now
    
    * T227: Removed deprecated generate_nav_html
    
    * T227: Revive 404
    
    * T227: Disable doc deployment testing
    
    ---------
    
    Co-authored-by: Hiroki Takezawa <hiroki.takezawa@ansys.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,7 +1,8 @@
 """Sphinx documentation configuration file."""
 from datetime import datetime
+import os
 
-from pyansys_sphinx_theme import pyansys_logo_black
+from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
 
 from ansys.edb import __version__
 
@@ -10,22 +11,39 @@ project = "ansys-edb"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "ANSYS, Inc."
 release = version = __version__
+cname = os.getenv("DOCUMENTATION_CNAME", "<DEFAULT_CNAME>")
 
 # use the default pyansys logo
 html_logo = pyansys_logo_black
-html_theme = "pyansys_sphinx_theme"
+html_theme = "ansys_sphinx_theme"
+html_short_title = html_title = "PyEDB"
 
-html_short_title = html_title = "ansys-edb"
+html_context = {
+    "github_user": "pyansys",
+    "github_repo": "pyedb",
+    "github_version": "develop",
+    "doc_path": "doc/source",
+}
 
-# specify the location of your github repo
 html_theme_options = {
+    "switcher": {
+        "json_url": f"https://{cname}/versions.json",
+        "version_match": get_version_match(__version__),
+    },
+    "check_switcher": False,
     "github_url": "https://github.com/pyansys/pyedb",
     "show_prev_next": False,
     "show_breadcrumbs": True,
+    "collapse_navigation": True,
+    "use_edit_page_button": True,
     "additional_breadcrumbs": [
         ("PyAnsys", "https://docs.pyansys.com/"),
     ],
 }
+
+html_favicon = ansys_favicon
+
+html_static_path = ["_static"]
 
 # Sphinx extensions
 extensions = [
@@ -72,9 +90,6 @@ numpydoc_validation_checks = {
 # notfound.extension
 notfound_template = "404.rst"
 notfound_urls_prefix = "/../"
-
-# static path
-html_static_path = ["_static"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
--- doc/source/user_guide/user_guide.rst
+++ doc/source/user_guide/user_guide.rst
@@ -3,11 +3,6 @@ User Guide
 
 **[Ansys Internal - Confidential]**
 
-The classes and functions are in the **ansys.edb** namespace
+The classes and functions are in the **ansys.edb** package.
 
-The Electronics Database (EDB) API allows access and manipulation of EDB databases. Design types managing Layout data in ANSYS Electronics Desktop have a corresponding aedb directory alongside the aedt file -- the aedb directory contains the EDB database. EDB databases can also be created by ANSYS translators, or by using these API functions directly; the resulting EBD can then be imported into ANSYS Electronics Desktop. 
-
-
-
-
-
+The Electronics Database (EDB) API allows access and manipulation of EDB databases. Design types managing Layout data in Ansys Electronics Desktop have a corresponding aedb directory alongside the AEDT file -- the aedb directory contains the EDB database. EDB databases can also be created by Ansys translators, or by using these API functions directly; the resulting EDB can then be imported into Ansys Electronics Desktop

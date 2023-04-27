7f9a44e6e1e3f252e597b47c70edd91bd0bb4461
drewm102 <103221973+drewm102@users.noreply.github.com>
2023-04-27 13:04:35 -0600

    D256: Models are created as derive type (#259)

--- src/ansys/edb/definition/component_property.py
+++ src/ansys/edb/definition/component_property.py
@@ -1,10 +1,10 @@
 """Component Property."""
 
 from ansys.api.edb.v1.component_property_pb2_grpc import ComponentPropertyServiceStub
+import ansys.api.edb.v1.model_pb2 as model_pb2
 
 from ansys.edb.core import ObjBase, messages
 from ansys.edb.definition import package_def
-from ansys.edb.hierarchy import model
 from ansys.edb.session import StubAccessor, StubType
 from ansys.edb.utility import Value
 
@@ -52,7 +52,23 @@ class ComponentProperty(ObjBase):
 
         A copy is returned. Use the setter for any modifications to be reflected.
         """
-        return model.Model(self.__stub.GetModel(messages.edb_obj_message(self)))
+        comp_model_msg = self.__stub.GetModel(messages.edb_obj_message(self))
+
+        def get_model_obj_type():
+            from ansys.edb.hierarchy import NetlistModel, PinPairModel, SParameterModel, SPICEModel
+
+            if comp_model_msg.model_type == model_pb2.SPICE_MODEL_TYPE:
+                return SPICEModel
+            elif comp_model_msg.model_type == model_pb2.S_PARAM_MODEL_TYPE:
+                return SParameterModel
+            elif comp_model_msg.model_type == model_pb2.PIN_PAIR_RLC_MODEL_TYPE:
+                return PinPairModel
+            elif comp_model_msg.model_type == model_pb2.NETLIST_MODEL_TYPE:
+                return NetlistModel
+            else:
+                raise TypeError("Unsupported model type.")
+
+        return get_model_obj_type()(comp_model_msg.model)
 
     @model.setter
     def model(self, value):

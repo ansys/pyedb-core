7511ed5a72775781400f4e66d8c5de9735676647
drewm102 <103221973+drewm102@users.noreply.github.com>
2025-03-28 09:07:44 -0600

    FIX: Fixed up layout doc. Updated conf.py. (#505)

--- src/ansys/edb/core/typing/__init__.py
+++ src/ansys/edb/core/typing/__init__.py
@@ -3,9 +3,11 @@
 from typing import Iterable, Tuple, Union
 
 import ansys.edb.core.geometry.point_data as point_data
+from ansys.edb.core.layer.layer import Layer
 from ansys.edb.core.utility.value import Value
 
 ValueLike = Union[int, float, complex, str, Value]
 PointLike = Union[point_data.PointData, Iterable[ValueLike]]
 Point3DLike = Tuple[ValueLike, ValueLike, ValueLike]
 Triangle3DLike = Tuple[Point3DLike, Point3DLike, Point3DLike]
+LayerLike = Union[Layer, str]

601dde9b5fb39e215e7aa3dcf651e7933e53631e
drewm102 <103221973+drewm102@users.noreply.github.com>
2025-05-01 17:40:17 -0600

    FIX: Add missing comp def APIs (#533)

--- src/ansys/edb/core/definition/component_def.py
+++ src/ansys/edb/core/definition/component_def.py
@@ -1,9 +1,23 @@
 """Component definition."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from src.ansys.edb.core.database import ProductIdType
+
 from ansys.api.edb.v1.component_def_pb2_grpc import ComponentDefServiceStub
 
 from ansys.edb.core.definition import component_model, component_pin
 from ansys.edb.core.edb_defs import DefinitionObjType
 from ansys.edb.core.inner import ObjBase, messages
+from ansys.edb.core.inner.messages import (
+    edb_obj_collection_property_message,
+    edb_obj_pair_message,
+    get_product_property_ids_message,
+    get_product_property_message,
+    set_product_property_message,
+)
 from ansys.edb.core.inner.utils import map_list
 from ansys.edb.core.session import StubAccessor, StubType
 
@@ -125,3 +139,80 @@ class ComponentDef(ObjBase):
         Once a component model is added to one component def, it cannot be added to any other, even when removed.
         """
         self.__stub.RemoveComponentModel(messages.pointer_property_message(self, value))
+
+    def reorder_pins(self, reordered_pins: List[component_pin.ComponentPin]):
+        """Reorders the existing pins in the components definition to be in the same order \
+        as in the provided list.
+
+        Parameters
+        ----------
+        reordered_pins : list of .ComponentPin
+            The component pins in the new order. These must be the same pins \
+            that already exist in the component definition.
+        """
+        self.__stub.ReorderPins(edb_obj_collection_property_message(self, reordered_pins))
+
+    def remove_pin(self, pin_to_remove: component_pin.ComponentPin):
+        """Remove the provided component pin from the component definition. \
+        the pin will be deleted and set to :meth:`null <.is_null>`.
+
+        Parameters
+        ----------
+        pin_to_remove : .ComponentPin
+            The pin to be removed.
+        """
+        self.__stub.RemovePin(edb_obj_pair_message(self, pin_to_remove))
+        pin_to_remove.msg = None
+
+    def get_product_property(self, prod_id: ProductIdType, attr_it: int) -> str:
+        """Get the product property for a given product ID and attribute ID.
+
+        Parameters
+        ----------
+        prod_id : .ProductIdType
+            Product ID.
+        attr_it : int
+            Attribute ID.
+
+        Returns
+        -------
+        str
+            Product property for the given product ID and attribute ID.
+        """
+        return self.__stub.GetProductProperty(
+            get_product_property_message(self, prod_id, attr_it)
+        ).value
+
+    def set_product_property(self, prod_id: ProductIdType, attr_it: int, prop_value: str):
+        """Set the product property for the given product ID and attribute ID.
+
+        Parameters
+        ----------
+        prod_id : .ProductIdType
+            Product ID.
+        attr_it : int
+            Attribute ID.
+        prop_value : str
+            New value for the product property.
+        """
+        self.__stub.SetProductProperty(
+            set_product_property_message(self, prod_id, attr_it, prop_value)
+        )
+
+    def get_product_property_ids(self, prod_id: ProductIdType) -> List[int]:
+        """Get the list of property IDs for a given property ID.
+
+        Parameters
+        ----------
+        prod_id : ProductIdType
+            Product ID.
+
+        Returns
+        -------
+        list[int]
+            Attribute IDs for the given product ID.
+        """
+        attr_ids = self.__stub.GetProductPropertyIds(
+            get_product_property_ids_message(self, prod_id)
+        ).ids
+        return [attr_id for attr_id in attr_ids]

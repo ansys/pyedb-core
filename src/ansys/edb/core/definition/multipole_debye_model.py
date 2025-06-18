a76c681da784c260e2ecf5a37dc49422f72ce3ae
dmiller <drew.miller@ansys.com>
2025-06-18 16:09:41 -0600

    DOC: material_property_thermal_modifier, multipole_debye_model, package_def, padstack_def

--- src/ansys/edb/core/definition/multipole_debye_model.py
+++ src/ansys/edb/core/definition/multipole_debye_model.py
@@ -1,4 +1,8 @@
 """Dielectric material definition."""
+from __future__ import annotations
+
+from typing import List, Tuple
+
 from ansys.api.edb.v1 import multipole_debye_model_pb2_grpc
 import ansys.api.edb.v1.multipole_debye_model_pb2 as pb
 from google.protobuf import empty_pb2
@@ -9,33 +13,30 @@ from ansys.edb.core.inner import messages
 
 
 class MultipoleDebyeModel(DielectricMaterialModel):
-    """Represents a dielectric material model."""
+    """Represents a multipole Debye dielectric material model."""
 
     __stub: multipole_debye_model_pb2_grpc.MultipoleDebyeModelServiceStub = session.StubAccessor(
         session.StubType.multipole_debye_model
     )
 
     @classmethod
-    def create(cls):
+    def create(cls) -> MultipoleDebyeModel:
         """Create a multipole Debye dielectric material model.
 
         Returns
         -------
-        MultipoleDebyeModel
+        .MultipoleDebyeModel
         """
         return MultipoleDebyeModel(cls.__stub.Create(empty_pb2.Empty()))
 
-    def get_parameters(self):
-        """Get parameters used to define the model.
+    @property
+    def parameters(self) -> Tuple[List[float], List[float], List[float]]:
+        """:obj:`tuple` of (:obj:`list` of :obj:`float`, :obj:`list` of :obj:`float`, :obj:`list` of :obj:`float`): \
+        Get parameters used to define the model.
 
-        Returns
-        -------
-        frequencies : list[float]
-            List of frequencies.
-        permitivities : list[float]
-            List of relative permitivites at each frequency.
-        loss_tangents : list[float]
-            List of loss tangents at each frequency.
+        The tuple is of the form ``(frequencies, relative_permittivities, dielectric_loss_tangents)`` \
+        where each relative permittivity and dielectric loss tangent value map to the frequency at the \
+        same index as itself.
         """
         parameters_msg = self.__stub.GetParameters(messages.edb_obj_message(self))
         return (
@@ -44,21 +45,11 @@ class MultipoleDebyeModel(DielectricMaterialModel):
             [i.value for i in parameters_msg.loss_tangents],
         )
 
-    def set_parameters(self, frequencies, permitivities, loss_tangents):
-        """Set parameters used to define the model.
-
-        Parameters
-        ----------
-        frequencies : list[float]
-            List of frequencies.
-        permitivities : list[float]
-            List of relative permitivites at each frequency.
-        loss_tangents : list[float]
-            List of loss tangents at each frequency.
-        """
-        frequencies_msg = [messages.double_message(i) for i in frequencies]
-        permitivities_msg = [messages.double_message(i) for i in permitivities]
-        loss_tangents_msg = [messages.double_message(i) for i in loss_tangents]
+    @parameters.setter
+    def parameters(self, params: Tuple[List[float], List[float], List[float]]):
+        frequencies_msg = [messages.double_message(i) for i in params[0]]
+        permitivities_msg = [messages.double_message(i) for i in params[1]]
+        loss_tangents_msg = [messages.double_message(i) for i in params[2]]
         self.__stub.SetParameters(
             pb.MultipoleDebyeModelSetParams(
                 target=self.msg,

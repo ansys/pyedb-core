3c39ce2b1e8b70cac1f99de6eca98b30476c42b8
Isaac Waldron <isaac.waldron@ansys.com>
2025-08-20 09:35:31 -0400

    Add test for multiple frequency ranges in SIwave sweep

---
+++ tests/e2e/integration_tests/test_siwave_simulation_setup.py
@@ -0,0 +1,15 @@
+from ansys.edb.core.layout.cell import Cell
+from ansys.edb.core.simulation_setup.simulation_setup import Distribution, FrequencyData, SweepData
+from ansys.edb.core.simulation_setup.siwave_simulation_setup import SIWaveSimulationSetup
+
+
+def test_create_siwave_simulation_setup(circuit_cell: Cell):
+    setup = SIWaveSimulationSetup.create(circuit_cell, "Setup1")
+    ff = [
+        FrequencyData(Distribution.LINC, "0GHz", "1GHz", 1001),
+        FrequencyData(Distribution.DEC, "1kHz", "1GHz", 20),
+    ]
+    sweep_data = SweepData("Sweep1", ff)
+    assert isinstance(sweep_data.frequency_data, list)
+    setup.sweep_data = [sweep_data]
+    assert isinstance(setup.sweep_data[0].frequency_data, list)

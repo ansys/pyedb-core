acb22f64887bdf54f9979edc7b89fb2f84f4d323
Isaac Waldron <isaac.waldron@ansys.com>
2025-08-20 10:01:56 -0400

    Improve test assertions

--- tests/e2e/integration_tests/test_siwave_simulation_setup.py
+++ tests/e2e/integration_tests/test_siwave_simulation_setup.py
@@ -6,10 +6,16 @@ from ansys.edb.core.simulation_setup.siwave_simulation_setup import SIWaveSimula
 def test_create_siwave_simulation_setup(circuit_cell: Cell):
     setup = SIWaveSimulationSetup.create(circuit_cell, "Setup1")
     ff = [
-        FrequencyData(Distribution.LINC, "0GHz", "1GHz", 1001),
-        FrequencyData(Distribution.DEC, "1kHz", "1GHz", 20),
+        FrequencyData(Distribution.LINC, "0GHz", "1GHz", "1001"),
+        FrequencyData(Distribution.DEC, "1kHz", "1GHz", "20"),
     ]
     sweep_data = SweepData("Sweep1", ff)
     assert isinstance(sweep_data.frequency_data, list)
     setup.sweep_data = [sweep_data]
     assert isinstance(setup.sweep_data[0].frequency_data, list)
+    for i, fd in enumerate(ff):
+        fd_read = setup.sweep_data[0].frequency_data[i]
+        assert fd.distribution == fd_read.distribution
+        assert fd.start_f == fd_read.start_f
+        assert fd.end_f == fd_read.end_f
+        assert fd.step == fd_read.step
